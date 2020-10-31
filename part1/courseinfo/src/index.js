056278fd2f4bbbdaad48b889c45374d3788c9c10
Liam <liamgwhittaker@gmail.com>
2020-10-31 18:38:31 +0000

    excersize 1.2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,9 +12,17 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <Part part={props.part1} excersize={props.exercises1} />
+      <Part part={props.part2} excersize={props.exercises2} />
+      <Part part={props.part3} excersize={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
+      <p>{props.part} {props.excersize}</p>
     </div>
   )
 }
